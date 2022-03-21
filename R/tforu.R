588a355818c2d5b1433a0163575264275495f159
EnriqueMD <s220670@dtu.dk>
2022-03-21 10:32:22 +0000

    fun 2

--- R/tforu.R
+++ R/tforu.R
@@ -1,10 +1,7 @@
-<<<<<<< HEAD
+
 usethis::use_r("tforu")
-=======
-?usethis::use_r("tforu")
 
 
->>>>>>> 640d530f8ab259db70f16c3d91059eecd89952f8
 #' DNA T to U conversion
 #'
 #' @param dnaseq
@@ -17,9 +14,8 @@ tforu <- function(dnaseq){
   rnaseq <- gsub("T", "U", dnaseq)
   return(rnaseq)
 }
-<<<<<<< HEAD
+
 ?tforu
 
 
-=======
->>>>>>> 640d530f8ab259db70f16c3d91059eecd89952f8
+
